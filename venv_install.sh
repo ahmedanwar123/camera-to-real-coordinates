312ccfa580afc1e8b69756ecd58aa45173794d66
Ahmed Anwar Gad <ahmed.anwar2003@gmail.com>
2025-01-09 19:25:37 +0200

    Update venv_install.sh

--- venv_install.sh
+++ venv_install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# To make this script executable, run:
-# chmod +x filename.sh
-
-# To execute this script, run:
-# ./filename.sh
-
 # Create a virtual environment named coord_trans
 python3 -m venv coord_trans
 
